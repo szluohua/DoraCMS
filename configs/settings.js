31b6e8ff99e8309e0aa962332d1067c1345e1964
hua <luohuasz@163.com>
2018-09-05 17:05:17 +0800

    add pm2.json

--- configs/settings.js
+++ configs/settings.js
@@ -8,7 +8,7 @@ module.exports = {
     session_secret: 'doracms', // 务必修改
     auth_cookie_name: 'doracms',
     cache_maxAge: Math.floor(Date.now() / 1000) + 24 * 60 * 60, //1 hours
-    serverPort: 8080,
+    serverPort: Number(process.env.PORT),
     lang: 'zh-CN', // 设置默认语言
     languages: ['zh-CN', 'ja_jp', 'en'], // 可选语言
 
@@ -22,8 +22,8 @@ module.exports = {
     DB: 'doracms2',
     HOST: '127.0.0.1',
     PORT: 27017,
-    USERNAME: 'doracms',
-    PASSWORD: 'password',
+    USERNAME: process.env.dbUsername,
+    PASSWORD: process.env.dbPassword,
 
     // 七牛配置
     openqn: false, //是否开启,若为true 则下面的信息必须配置正确完整
@@ -35,7 +35,7 @@ module.exports = {
     assetsCdn: true, // 静态资源使用cnd.请在build完成后将 elemt.*.js 上传的七牛的融合cdn
 
     // redis配置
-    openRedis: true, //是否开启,若为true 则下面的信息必须配置正确完整
+    openRedis: false, //是否开启,若为true 则下面的信息必须配置正确完整
     redis_host: '127.0.0.1',
     redis_port: 6379,
     redis_psd: 'your redis password',
