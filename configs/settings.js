332825c7ceb034ed0aca8a7588b39770a5b37ad1
hua <luohuasz@163.com>
2018-09-05 23:28:52 +0800

    update

--- configs/settings.js
+++ configs/settings.js
@@ -38,7 +38,7 @@ module.exports = {
     openRedis: false, //是否开启,若为true 则下面的信息必须配置正确完整
     redis_host: '127.0.0.1',
     redis_port: 6379,
-    redis_psd: 'your redis password',
+    redis_psd: process.env.redisPassword,
     redis_db: 0,
     redis_ttl: 24 * 30, // 过期时间30天
 
